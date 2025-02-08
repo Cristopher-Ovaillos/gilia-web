5c015ce95092abf3eb1968979d7579ab847630fa
sebareibold <sebastianreibold2003@gmail.com>
2025-02-08 15:49:16 -0300

    Creacion de la barra de Navegacion (En proceso)

---
+++ pagina-web-gilia/src/Assets/styles/themes.ts
@@ -0,0 +1,19 @@
+
+
+export const lightTheme = {
+    token: {
+        colorPrimario: '#1890ff',
+        backgroundColor: '#ffffff',
+        colorTextBase: '#000000',
+        // otros tokens para el tema claro...
+    }
+};
+
+export const darkTheme = {
+    token: {
+        colorPrimario: '#1d1d1d',
+        backgroundColor: '#121212',
+        colorTextBase: '#ffffff',
+        // otros tokens para el tema oscuro...
+    }
+};
