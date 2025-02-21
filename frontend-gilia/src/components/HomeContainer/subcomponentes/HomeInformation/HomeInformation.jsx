48fa83e34ff0826a7fc5237db592505cf0728937
sebareibold <sebastianreibold2003@gmail.com>
2025-02-21 10:47:16 -0300

    Actualizacion del FrontEnd

---
+++ frontend-gilia/src/components/HomeContainer/subcomponentes/HomeInformation/HomeInformation.jsx
@@ -0,0 +1,34 @@
+import "./HomeInformation.css";
+
+ const HomeInformation = () => {
+  return (
+    <div
+    className="relative isolate bg-cover bg-fixed bg-center bg-opacity-50"
+    style={{ backgroundImage: 'url("")' }}
+  >
+    <div className="mx-auto max-w-2xl sm:py-24 lg:py-34">
+      <div className="text-center">
+        <h1 className="custom-color text-7xl font-semibold tracking-tight text-balance sm:text-6xl">
+          {" "}
+          Investigación en Lenguajes e Inteligencia Artificial{" "}
+        </h1>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="#"
+            className="rounded-md custom-color custom-back px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
+          >
+            Get started
+          </a>
+          <a href="#"
+            className=" custom-color text-sm/6 font-semibold text-gray-900"
+          >
+            Learn more <span aria-hidden="true">→</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default HomeInformation;
