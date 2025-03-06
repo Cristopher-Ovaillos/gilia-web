70ef425615884d17dbce935f366187112d5f5cca
sebareibold <sebastianreibold2003@gmail.com>
2025-03-06 08:49:33 -0300

    Modificaciones en Home

--- frontend-gilia/src/components/Home/subcomponentes/CardExploration/Card.jsx
+++ frontend-gilia/src/components/Home/subcomponentes/CardExploration/Card.jsx
@@ -6,20 +6,20 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-const Card = ({ id, name, autor, description, onClickTo, featured, href }) => {
+// eslint-disable-next-line react/prop-types
+const Card = ({ titulo, descripcion, link, activa }) => {
   return (
-    <div className={classNames(featured ? 'custom-card-active' : 'custom-card-inactive mt-[10%]', ' custom-card-home custom-animation-start')} >
+    <div className={classNames(activa ? 'custom-card-active' : 'custom-card-inactive mt-[10%]', ' custom-card-home custom-animation-start')} >
       <div className="absolute top-0 inset-x-0 h-1.5 bg-indigo-600 rounded-t-lg" aria-hidden="true" />
-      <h3 id={id} className={classNames(featured ? ' text-2xl text-white' : ' text-1xl text-gray-300 ',' font-semibold  ', 'text-center custom-text-card'   )}> {name} </h3>
-      <p className="flex-start items-baseline ">   <span className="custom-autor-card"> {autor} </span> </p>
-      <p className="mt-6 custom-descrip-card">{description}</p>
-      <a href={href}   aria-describedby={id} className={classNames(
-          featured
+      <h3 id={titulo} className={classNames(activa ? ' text-2xl text-white' : ' text-1xl text-gray-300 ',' font-semibold  ', 'text-center custom-text-card'   )}> {titulo} </h3>
+      <p className="mt-6 custom-descrip-card">{descripcion}</p>
+      <a href={link}   aria-describedby={titulo} className={classNames(
+          activa
             ? 'btn-active'
             : 'btn-inactive',
           'custom-btn'
         )}
-        onClick={onClickTo}
+        onClick={link}
       >Ver más</a>
     </div>
   );
